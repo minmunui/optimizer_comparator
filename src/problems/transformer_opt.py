1b9b976d596c11071519c878f529d8a60d1bf754
DongjinPark <ehdwls1638@pusan.ac.kr>
2025-03-19 22:50:02 +0900

    feat : complete practical problem

--- src/problems/transformer_opt.py
+++ src/problems/transformer_opt.py
@@ -16,50 +16,45 @@ WEIGHT_FOR_ENS = 1.0 / 3.0
 WEIGHT_FOR_SAIFI = 1.0 / 3.0
 
 
-class FacilityNode:
+class SchematicNode:
     num_facility = 0
+    loads = []
+    machines = []
 
     def __init__(self,
                  name: str = None,
+                 children: list['SchematicNode'] = None,
+                 parent: 'SchematicNode' = None,
                  strategy_variables: list[Variable] = None,
-                 outage_rates: list[int] = None,
-                 average_outage_time: int = None,
+                 outage_rates: list[float] = None,
+                 average_outage_time: float = None,
                  is_load: bool = False):
-        if len(outage_rates) == NUM_STRATEGY and len(strategy_variables) == NUM_STRATEGY:
-            raise ValueError(f"Length of outage_rates and strategy_variables should be the same as NUM_STRATEGY"
-                             f"NUM_STRATEGY : ({NUM_STRATEGY})"
-                             f"Length of outage_rates : ({len(outage_rates)})"
-                             f"Length of strategy_variables : ({len(strategy_variables)})")
 
-        FacilityNode.num_facility += 1
-        self.id = FacilityNode.num_facility
-        self.name = name if name is not None else f'Facility_{FacilityNode.num_facility}'
-        self.outage_rates = outage_rates
-        self.average_outage_time = average_outage_time
-        self.is_load = is_load
-        self.strategy_variables = strategy_variables
+        if children is None:
+            self.children = []
+        elif isinstance(children, list):
+            self.children = children
+            for child in children:
+                child.parent = self
 
-    def __repr__(self):
-        return f'FacilityNode({self.outage_rates}, {self.average_outage_time}, {self.is_load})'
-
-    def __str__(self):
-        return f'FacilityNode({self.outage_rates}, {self.average_outage_time}, {self.is_load})'
-
-    def get_outage_rate_variable(self):
-        return sum(
-            [outage_rate * variable for outage_rate, variable in zip(self.outage_rates, self.strategy_variables)])
-
-
-class SchematicNode:
-    def __init__(self, value: FacilityNode, children: list['SchematicNode'] = None, parent: 'SchematicNode' = None):
-        self.value = value
-        self.children = children if children is not None else []
+        if parent is not None:
+            parent.add_child(self)
         self.parent = parent
 
-    def __repr__(self):
-        return f'SchematicNode({self.value})'
+        self.is_load = is_load
+        if is_load:
+            SchematicNode.loads.append(self)
 
-    def add_child(self, child: 'SchematicNode' | list['SchematicNode']):
+        SchematicNode.machines.append(self)
+        SchematicNode.num_facility += 1
+        self.id = SchematicNode.num_facility
+        self.name = name if name is not None else f'Facility_{SchematicNode.num_facility}'
+
+        self.outage_rates = outage_rates
+        self.average_outage_time = average_outage_time
+        self.strategy_variables = strategy_variables
+
+    def add_child(self, child):
         if isinstance(child, list):
             self.children.extend(child)
             for _child in child:
@@ -67,7 +62,7 @@ class SchematicNode:
         elif isinstance(child, SchematicNode):
             self.children.append(child)
 
-        else :
+        else:
             raise ValueError(f"child should be SchematicNode or list[SchematicNode]")
         child.parent = self
 
@@ -83,42 +78,54 @@ class SchematicNode:
         return self.parent.trace_root() + [self]
 
     def trace_outage_rate(self) -> float:
-        return sum([facility.value.outage_rates for facility in self.trace_root() if not facility.value.is_load])
+        return sum([facility.outage_rates for facility in self.trace_root() if not facility.is_load])
 
     def trace_outage_rate_variable(self):
-        linked_facilities = [facility for facility in self.trace_root() if not facility.value.is_load]
-        outage_rate_variable = sum([facility.value.get_outage_rate_variable() for facility in linked_facilities])
+        linked_facilities = [facility for facility in self.trace_root() if not facility.is_load]
+        outage_rate_variable = sum([facility.get_outage_rate_variable() for facility in linked_facilities])
         return outage_rate_variable
 
     def trace_average_repair_time(self) -> float:
-        average_repair_time = [facility.value.average_outage_time for facility in self.trace_root() if
-                               not facility.value.is_load]
+        average_repair_time = [facility.average_outage_time for facility in self.trace_root() if
+                               not facility.is_load]
         if len(average_repair_time) == 0:
             return 0
         return sum(average_repair_time) / len(average_repair_time)
 
+    def get_outage_rate_variable(self):
+        return sum(
+            [outage_rate * variable for outage_rate, variable in zip(self.outage_rates, self.strategy_variables)])
+
     def remove_child(self, child):
         self.children.remove(child)
+
+    def print_tree(self, depth=0):
+        print(' ' * depth, self.name)
+        for child in self.children:
+            child.print_tree(depth + 1)
 
 
 def make_cost_constraint(solver: pywraplp.Solver,
                          strategy_variables: list[list[Variable]],
                          strategy_cost: list[list[int]],
                          max_cost: int):
-    for _machine in range(NUM_MACHINE):
-        solver.Add(sum(strategy_cost[_machine][_strategy] * strategy_variables[_machine][_strategy] for _strategy in
-                       range(NUM_STRATEGY)) <= max_cost)
+    return [solver.Add(sum(strategy_cost[_machine][_strategy] * strategy_variables[_machine][_strategy] for _strategy in
+                           range(len(strategy_variables[0]))) <= max_cost) for _machine in
+            range(len(strategy_variables))]
 
 
-def make_strategy_constraint(solver: pywraplp.Solver) -> list[list[Variable]]:
-    x = list([0] * NUM_STRATEGY for _ in range(NUM_MACHINE))  # list[ list[0, 0, 0, 0], list[0, 0, 0, 0], ... ]
-    for _machine in range(NUM_STRATEGY):
-        for _strategy in range(NUM_STRATEGY):
+def make_strategy_constraint(solver: pywraplp.Solver,
+                             num_strategy: int = NUM_STRATEGY,
+                             num_machine: int = NUM_MACHINE) \
+        -> list[list[Variable]]:
+    x = list([0] * num_strategy for _ in range(num_machine))  # list[ list[0, 0, 0, 0], list[0, 0, 0, 0], ... ]
+    for _machine in range(num_machine):
+        for _strategy in range(num_strategy):
             x[_machine][_strategy] = solver.IntVar(0, 1,
                                                    f'x_{_machine}_{_strategy}')  # each _strategy is binary variable
 
-    for _machine in range(NUM_MACHINE):
-        solver.Add(sum(x[_machine][_strategy] for _strategy in range(NUM_STRATEGY)) == 1)
+    for _machine in range(num_machine):
+        solver.Add(sum(x[_machine][_strategy] for _strategy in range(num_strategy)) == 1)
 
     return x
 
