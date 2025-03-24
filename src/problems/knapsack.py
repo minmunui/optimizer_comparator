34a9881c0dc151b2dd80c6c42b484cf16874debc
DongjinPark <ehdwls1638@pusan.ac.kr>
2025-03-24 15:56:49 +0900

    feat: update eval_knapsack function to include rag parameter and remove redundant function

--- src/problems/knapsack.py
+++ src/problems/knapsack.py
@@ -34,30 +34,16 @@ def generate_knapsack_problem(num_items: int = 80,
     return weights, values, capacity
 
 
-def eval_knapsack(x: list[int], values: list[int], weights: list[int], capacity: int, division: int = 1):
-    x_binaries = np.round(x).astype(int)
+def eval_knapsack(x: list[int], values: list[int], weights: list[int], capacity: int, division: int = 1,
+                  rag: bool = False):
     total_value = 0
     total_weight = 0
-    for i, item in enumerate(x_binaries):
+    for i, item in enumerate(x):
         if item > division:
             raise ValueError(f"Division value should be less than {division} : {i}-{item}")
         total_value += item * values[i] / division
         total_weight += item * weights[i] / division
 
     if total_weight > capacity:
-        return -1 * (total_weight - capacity)
+        return -1 * (total_weight - capacity) if rag else 0
     return total_value
-
-
-def eval_knapsack_rag(individual: list[int], values: list[int], weights: list[int], capacity: int, division: int = 1):
-    total_value = 0
-    total_weight = 0
-    for i, item in enumerate(individual):
-        if item > division:
-            raise ValueError(f"Division value should be less than {division} : {i}-{item}")
-        total_value += item * values[i] / division
-        total_weight += item * weights[i] / division
-
-    if total_weight > capacity:
-        return capacity - (total_weight - capacity),
-    return total_value,
